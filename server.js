d3c850e74df7b4bc4c955ff60ab82652ee4945ab
mischa.huang <mischa.huang@creativearktech.com>
2022-02-28 14:28:25 +0800

    feat: add post api (test)

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const http = require('http');
+const { v4: uuidv4 } = require('uuid');
 
 const config = require('./config');
 
 const API_ENV = config.API_ENV;
 const headers = config.headers;
 
+const todos = [{ 
+  id: uuidv4(),
+  title: 'eat'
+}];
+
 const requestListener = (req, res) => {
   if(req.url === API_ENV && req.method === 'GET') {
     res.writeHead(200, headers);
     res.write(JSON.stringify({
       'status': 'success',
-      'data': []
+      'data': todos
     }));
+    res.end();
+  } else if(req.url === API_ENV && req.method === 'POST') {
+    res.writeHead(200, headers);
+    res.write(JSON.stringify({
+      'status': 'success',
+      'data': todos
+    }))
     res.end();
   } else if(req.method === 'OPTIONS') {
     res.writeHead(200, headers);
