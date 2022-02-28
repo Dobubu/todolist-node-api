6bb4e56ca2a79cdd415a9f776440aeedd44a6465
mischa.huang <mischa.huang@creativearktech.com>
2022-02-28 14:10:24 +0800

    feat: add 404 url page

---
+++ config.js
@@ -0,0 +1,10 @@
+const API_ENV = '/todos';
+
+const headers = {
+  'Content-Type': 'text/plain'
+};
+
+module.exports = {
+  API_ENV,
+  headers
+};
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
+const config = require('./config');
+
+const API_ENV = config.API_ENV;
+const headers = config.headers;
+
 const requestListener = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text-plain'});
-  res.write('hello');
-  res.end();
+  if(req.url === API_ENV && req.method === 'GET') {
+    res.writeHead(200, headers);
+    res.write('index');
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write('page not found!');
+    res.end();
+  };
 };
 
 const server = http.createServer(requestListener);
